2a73498379aa6a1fb10121306eb95a321b4b157e
weejo <91460362+weejo@users.noreply.github.com>
2022-05-31 09:47:10 +0200

    Cleanup, Rename, and some tweaks

---
+++ Source/DeadSapling/ProjectileBase.h
@@ -0,0 +1,25 @@
+﻿// ©2022 The Icecream Connection
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+UCLASS()
+class DEADSAPLING_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
